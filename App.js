fde51e56780289cc6b96e2f942bb3e49b2c599e6
davidasare2 <davidasare600.com>
2024-07-05 10:54:10 +0300

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View } from 'react-native';
+import { styled } from 'nativewind';
+
+const StyledView = styled(View);
+const StyledText = styled(Text);
 
 const MyComponent = () => {
   return (
-    <View className="flex flex-1 items-center justify-center">
-      <Text className="text-2xl">
-        My name is <Text className="font-bold">David</Text>
-      </Text>
-    </View>
+    <StyledView className="flex flex-1 items-center justify-center bg-green-500">
+      <StyledText className="text-2xl">
+        My name is <StyledText className="font-bold">David</StyledText>
+      </StyledText>
+    </StyledView>
   );
 }
 
